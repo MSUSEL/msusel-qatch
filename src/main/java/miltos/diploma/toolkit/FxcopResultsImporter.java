d266bb72b72d46388a774da577437744fbd3e045
montanarice <davidrice363@gmail.com>
2019-04-01 14:21:03 -0600

    FxcopResultsImporter and test shells added

--- src/main/java/miltos/diploma/toolkit/FxcopResultsImporter.java
+++ src/main/java/miltos/diploma/toolkit/FxcopResultsImporter.java
@@ -1,7 +1,20 @@
 package miltos.diploma.toolkit;
 
+/**
+ * This class is responsible for importing the issues found by FxCop 15.0
+ * in to a data structure.
+ */
 public class FxcopResultsImporter {
 
+    /**
+     * Parse issues in the Fxcop XML file that correspond to a property
+     * into an IssueSet object
+     *
+     * @param path
+     *      the directory location of the XML file
+     * @return
+     *      object representing all found violations of the current property
+     */
     public IssueSet parseIssues(String path) {
         throw new RuntimeException("TODO complete writing method");
     }
--- src/test/java/miltos/diploma/unittests/FxcopResultsImporterTest.java
+++ src/test/java/miltos/diploma/unittests/FxcopResultsImporterTest.java
@@ -1,12 +1,26 @@
 package miltos.diploma.unittests;
 
+import miltos.diploma.toolkit.FxcopResultsImporter;
+import miltos.diploma.toolkit.Issue;
+import miltos.diploma.toolkit.IssueSet;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class FxcopResultsImporterTest {
 
+    /**
+     * This method should successfully find the FxCop XML file, parse the found issues in to Issue objects,
+     * add the Issue objects to an IssueSet, and name the IssueSet according to the QM Property the findings belong to.
+     */
     @Test
-    public void testParseMetrics() {
+    public void testParseIssues() {
+        String resultsPath = "src/test/resources/scanner-results/FxcopFindingsProperty01.xml";
+        FxcopResultsImporter ri = new FxcopResultsImporter();
+        IssueSet is = ri.parseIssues(resultsPath);
+        Issue firstIssue = is.getIssues().firstElement();
+        Issue lastIssue = is.getIssues().lastElement();
 
+        Assert.assertEquals(is.getPropertyName(), "FxcopFindingsProperty01");
+        Assert.assertEquals(is.getIssues().size(),6);
     }
 }
