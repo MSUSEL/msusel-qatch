7e2856f074fa5a60746ee55c2392335ba0af0a5f
David Rice <davidrice363@gmail.com>
2019-04-04 15:00:42 -0600

    added mac os funcationality

--- src/main/java/miltos/diploma/toolkit/FxcopPriority.java
+++ src/main/java/miltos/diploma/toolkit/FxcopPriority.java
@@ -1,24 +1,10 @@
 package miltos.diploma.toolkit;
 
-import java.util.EnumSet;
-import java.util.HashMap;
-import java.util.Map;
-
 public enum FxcopPriority {
     WAITING(0),
     READY(1),
     SKIPPED(-1),
     Completed(5);
-
-    private static final Map<Integer, FxcopPriority> intLookup = new HashMap<>();
-    private static final Map<FxcopPriority, Integer> priorityLookup = new HashMap<>();
-
-    static {
-        for (FxcopPriority s : EnumSet.allOf(FxcopPriority.class)) {
-            intLookup.put(s.getCode(), s);
-            priorityLookup.put(s, s.getCode());
-        }
-    }
 
     private int code;
 
@@ -28,13 +14,5 @@ public enum FxcopPriority {
 
     public int getCode() {
         return code;
-    }
-
-    public static FxcopPriority get(int code) {
-        return intLookup.get(code);
-    }
-
-    public static int get(FxcopPriority code) {
-        return priorityLookup.get(code);
     }
 }
