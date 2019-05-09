add47f19705e419f41aeedd0c9bc15c5d4d97d37
montanarice <davidrice363@gmail.com>
2019-05-09 15:57:56 -0600

    SingleProjectEvaluator main class working on C# for IDE

--- src/main/java/miltos/diploma/SingleProjectEvaluator.java
+++ src/main/java/miltos/diploma/SingleProjectEvaluator.java
@@ -1,14 +1,18 @@
 package miltos.diploma;
 
 import miltos.diploma.calibration.BenchmarkAnalyzer;
+import miltos.diploma.evaluation.EvaluationResultsExporter;
 import miltos.diploma.evaluation.Project;
-import miltos.diploma.qualitymodel.QualityModel;
-import miltos.diploma.qualitymodel.QualityModelLoader;
+import miltos.diploma.evaluation.ProjectCharacteristicsEvaluator;
+import miltos.diploma.evaluation.ProjectEvaluator;
+import miltos.diploma.qualitymodel.*;
 import miltos.diploma.toolkit.*;
 import miltos.diploma.utility.ProjectInfo;
 import miltos.diploma.utility.ProjectLanguage;
 import org.apache.commons.io.FileUtils;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.*;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
@@ -37,7 +41,7 @@ public class SingleProjectEvaluator {
      *             1: path to folder to place results
      *    These arg paths can be relative or full path
      */
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException, CloneNotSupportedException {
 
         clean("resources", "config.properties");
 
@@ -146,6 +150,195 @@ public class SingleProjectEvaluator {
             System.out.println("* You can find the results at : " + BenchmarkAnalyzer.SINGLE_PROJ_RESULT_PATH);
             System.out.println();
         }
+
+        System.out.println("\n**************** STEP 3: Results Importer ****************************");
+        System.out.println("*");
+        System.out.println("* Importing the results of the analysis...");
+        System.out.println("* Please wait...");
+        System.out.println("*");
+
+        //(TODO): Refactor into Builder or Template design pattern and move into framework classes
+        //Create a simple PMD and CKJM Result Importers
+        MetricsResultsImporter metricsImporter;
+        FindingsResultsImporter findingsImporter;
+
+        if (projectLanguage == ProjectLanguage.Java) {
+            metricsImporter = new CKJMResultsImporter();
+            findingsImporter = new PMDResultsImporter();
+        }
+        else if (projectLanguage == ProjectLanguage.CSharp) {
+            metricsImporter = new LOCMetricsResultsImporter();
+            findingsImporter = new FxcopResultsImporter();
+        }
+        else throw new RuntimeException("projectLanguage did not match to a support language enumeration");
+
+        //Get the directory with the results of the analysis
+        File resultsDir = new File(BenchmarkAnalyzer.SINGLE_PROJ_RESULT_PATH + File.separator + project.getName());
+        File[] results = resultsDir.listFiles();
+
+        //For each result file found in the directory do...
+        if (results == null) throw new RuntimeException("Scanner results directory " + resultsDir.toString() + " has no files");
+        for(File resultFile : results){
+
+            //(TODO): Refactor into Builder or Template design pattern and move into framework classes
+            //Check if it is a ckjm result file
+            if(!resultFile.getName().contains("ckjm") && !resultFile.getName().contains("Loc")){
+                //Parse the issues and add them to the IssueSet Vector of the Project object
+                project.addIssueSet(findingsImporter.parse(resultFile.getAbsolutePath()));
+            }else{
+                //Parse the metrics of the project and add them to the MetricSet field of the Project object
+                project.setMetrics(metricsImporter.parse(resultFile.getAbsolutePath()));
+            }
+        }
+
+        // Print some informative messages to the console
+        System.out.println("*");
+        System.out.println("* The results of the static analysis are successfully imported ");
+
+
+        /*
+         * Step 4 : Aggregate the static analysis results of the desired project
+         */
+        System.out.println("\n**************** STEP 4: Aggregation Process *************************");
+
+        //Print some messages
+        System.out.println("*");
+        System.out.println("* Aggregating the results of the project...");
+        System.out.println("* I.e. Calculating the normalized values of its properties...");
+        System.out.println("* Please wait...");
+        System.out.println("*");
+
+        //Clone the properties of the quality model to the properties of the certain project
+        for(int i = 0; i < qualityModel.getProperties().size(); i++){
+            //Clone the property and add it to the PropertySet of the current project
+            Property p = (Property) qualityModel.getProperties().get(i).clone();
+            project.addProperty(p);
+        }
+
+        //(TODO): Refactor into design pattern
+        Aggregator metricsAggregator;
+        Aggregator findingsAggregator;
+        if (projectLanguage == ProjectLanguage.Java) {
+            metricsAggregator = new CKJMAggregator();
+            findingsAggregator = new PMDAggregator();
+        }
+        else if (projectLanguage == ProjectLanguage.CSharp) {
+            metricsAggregator = new LOCMetricsAggregator();
+            findingsAggregator = new FxcopAggregator();
+        }
+        else throw new RuntimeException("projectLanguage did not match to a support language enumeration");
+
+        //Aggregate all the analysis results
+        metricsAggregator.aggregate(project);
+        findingsAggregator.aggregate(project);
+
+        //Normalize their values
+        for(int i = 0; i < project.getProperties().size(); i++){
+            Property property =  project.getProperties().get(i);
+            property.getMeasure().calculateNormValue();
+        }
+
+        System.out.println("*");
+        System.out.println("* Aggregation process finished..!");
+
+
+        /*
+         * STEP 5 : Evaluate all the benchmark projects against their thresholds.
+         */
+
+        System.out.println("\n**************** STEP 5: Properties Evaluation ***********************");
+        System.out.println("*");
+        System.out.println("* Evaluating the project's properties against the calculated thresholds...");
+        System.out.println("* This will take a while...");
+        System.out.println("*");
+
+        //Create a single project property evaluator
+        ProjectEvaluator evaluator = new ProjectEvaluator();
+
+        //Evaluate all its properties
+        evaluator.evaluateProjectProperties(project);
+
+        System.out.println("*");
+        System.out.println("* The project's properties successfully evaluated..!");
+
+        /*
+         * Step 6 : Evaluate the project's characteristics
+         */
+
+        System.out.println("\n**************** STEP 6: Characteristics Evaluation ******************");
+        System.out.println("*");
+        System.out.println("* Evaluating the project's characteristics based on the eval values of its properties...");
+        System.out.println("* This will take a while...");
+        System.out.println("*");
+
+        //Clone the quality model characteristics inside the project
+        //For each quality model's characteristic do...
+        for(int i = 0; i < qualityModel.getCharacteristics().size(); i++){
+            //Clone the characteristic and add it to the CharacteristicSet of the current project
+            Characteristic c = (Characteristic) qualityModel.getCharacteristics().get(i).clone();
+            project.getCharacteristics().addCharacteristic(c);
+        }
+
+        //Create a single project property evaluator
+        ProjectCharacteristicsEvaluator charEvaluator = new ProjectCharacteristicsEvaluator();
+
+        //Evaluate the project's characteristics
+        charEvaluator.evaluateProjectCharacteristics(project);
+
+        System.out.println("*");
+        System.out.println("* The project's characteristics successfully evaluated..!");
+
+        /*
+         * Step 7 : Calculate the TQI of the project
+         */
+
+        System.out.println("\n**************** STEP 7: TQI Calculation *****************************");
+        System.out.println("*");
+        System.out.println("* Calgculating the TQI of the project ...");
+        System.out.println("* This will take a while...");
+        System.out.println("*");
+
+        //Copy the TQI object of the QM to the tqi field of this project
+        project.setTqi((Tqi)qualityModel.getTqi().clone());
+
+        //Calculate the project's TQI
+        project.calculateTQI();
+
+        System.out.println("*");
+        System.out.println("* The TQI of the project successfully evaluated..!");
+
+        /*
+         * Step 8 : Export the project's data and properties in a json file
+         */
+        System.out.println("\n**************** STEP 8: Exporting Evaluation Results ****************");
+        System.out.println("*");
+        System.out.println("* Exporting the results of the project evaluation...");
+        System.out.println("* This will take a while...");
+        System.out.println("*");
+
+        //Clear Issues and metrics for more lightweight solution
+        //TODO: Remove this ... For debugging purposes only
+        if(!Boolean.parseBoolean(properties.getProperty("output.inspectionresults"))){
+            project.clearIssuesAndMetrics();
+        }
+
+        EvaluationResultsExporter.exportProjectToJson(
+                project,
+                new File(properties.getProperty("results.location") + File.separator + project.getName() + "_evalResults.json")
+                        .getAbsolutePath()
+        );
+
+        System.out.println("* Results successfully exported..!");
+        System.out.println("* You can find the results at : " + new File(properties.getProperty("results.location")).getAbsolutePath());
+
+        /*
+         * Step 8.1 : Export the results to the predefined path as well
+         */
+        clean(EvaluationResultsExporter.SINGLE_PROJ_RESULT_PATH);
+        //Export the results
+        EvaluationResultsExporter.exportProjectToJson(project, new File(EvaluationResultsExporter.SINGLE_PROJ_RESULT_PATH + "/" + project.getName() + "_evalResults.json").getAbsolutePath());
+        System.out.println("* You can find the results at : " + new File(EvaluationResultsExporter.SINGLE_PROJ_RESULT_PATH).getAbsolutePath() + " as well..!");
+
     }
 
     private static void clean(String... filePaths) throws IOException {
